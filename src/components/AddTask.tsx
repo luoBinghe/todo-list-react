c48d20a174f635b1149ebe38d10e940d191420a1
guayana <isabelaguayanaz@gmail.com>
2022-09-20 21:01:34 -0300

    feat: implementando reducer com estado das tasks

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,16 +1,44 @@
-import { FormEvent } from 'react'
+import { FormEvent, useReducer, ChangeEvent } from 'react'
+import { ActionKind } from '../enums/Actions';
 import style from './AddTask.module.css'
 import Input from './Input'
 
+type State = {
+  task: string;
+}
+
+type Action = {
+  type: ActionKind,
+  payload: string
+}
+
+export const setValue: Action = {
+  type: ActionKind.setValue,
+  payload: ''
+}
+
+const initialArg: State = { task: '' }
+
+
 export default function AddTask(){
-  const handleCreateNewTask = (event: FormEvent<HTMLFormElement>) => {
-    event?.preventDefault()
+  const [state, dispatch] = useReducer(reducer, initialArg)
+
+  function reducer(state: State, action: Action): State {
+    switch(action.type){
+      case ActionKind.setValue:
+        return { ...state, task: action.payload }
+    }
   }
 
-    return (
-      <form onSubmit={(event) => handleCreateNewTask(event)} className={style.add}>
-        <Input />
-        <button type='submit'>Criar +</button>
-      </form>
-    )
+  const handleCreateNewTask = (event: FormEvent<HTMLFormElement>) => {
+    event?.preventDefault()
+    
+  }
+
+  return (
+    <form onSubmit={(event) => handleCreateNewTask(event)} className={style.add}>
+      <Input onChange={(event: any) => dispatch({type: ActionKind.setValue, payload: event.target.value})} value={state.task}/>
+      <button type='submit'>Criar +</button>
+    </form>
+  )
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,17 @@
+import { FC, InputHTMLAttributes } from 'react'
 import style from './Input.module.css'
 
-export default function Input(){
-    return (
-      <div className={style.container}>
-        <input type="text" placeholder='   Adicione uma nova tarefa' />
-      </div>
-    )
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
+  name?: string,
+  label?: string
 }
+
+const Input: FC<InputProps> = ({ name, label, ...rest }) => {
+  return (
+    <div className={style.container}>
+      <input {...rest} type="text" placeholder='   Adicione uma nova tarefa' />
+    </div>
+  )
+}
+
+export default Input;
